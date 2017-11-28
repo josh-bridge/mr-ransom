5b8c6805b24e0a5680b97c8a081e52cf0dc63305
josh-bridge <14032908@stu.mmu.ac.uk>
2017-11-28 20:09:09 +0000

    test xor

--- script/aldersonalgorithm.py
+++ script/aldersonalgorithm.py
@@ -11,20 +11,36 @@ def process(key, in_text, mode):
     return CaeserCipher(key, mode).process(in_text)
 
 
+def xor_string(string1, string2):
+    arr1 = [ord(c) for c in string1]
+    arr2 = [ord(c) for c in string2]
+
+    arr3 = []
+    for i in range(len(arr1)):
+        arr3.append(arr1[i] ^ arr2[i])
+
+    return "".join(map(chr, arr3))
+
+
 class AldersonAlgorithm:
     def __init__(self, key):
         self.key = key
-        self.previous = ""
+        self.previous = None
 
     def encrypt_chunk(self, chunk):
         result = process(self.key, base64.b64encode(chunk), ENCRYPT)
-        self.previous = result
 
+        if self.previous is not None:
+            result = xor_string(result, self.previous)
+
+        self.previous = result
         return result
 
     def decrypt_chunk(self, chunk):
-        result = base64.b64decode(process(self.key, chunk, DECRYPT))
+        result = process(self.key, chunk, DECRYPT)
+
+        if self.previous is not None:
+            result = xor_string(result, self.previous)
+
         self.previous = result
-
-        return result
-
+        return base64.b64decode(result)
